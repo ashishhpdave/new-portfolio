a2753db0ba1fde2f16fb72a990f49e9af209516c
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-08-08 11:49:40 +0530

    some-animastion-and-font-changed
    this is the code for some animastion and font style change........

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 tailwind.config = {
     theme:{
-        gridTemplateColumns:{
-            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
-        }
+
+        extend:{
+            gridTemplateColumns:{
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily:{
+                Outfit: ["Outfit", "sans-serif"],
+                Ovo: ["Ovo", "serif"]
+            },
+            animation:{
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors:{
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a'
+            }
+        }   
     }
 }
